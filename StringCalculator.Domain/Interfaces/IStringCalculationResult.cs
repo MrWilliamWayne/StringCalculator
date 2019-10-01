68308191264779bd33fd6cb420078326f49bae36
Will Wayne <jedidev@gmail.com>
2019-10-01 16:06:39 -0500

    Removed unnecessary interface method ProcessTerm

--- StringCalculator.Domain/Interfaces/IStringCalculationResult.cs
+++ StringCalculator.Domain/Interfaces/IStringCalculationResult.cs
@@ -1,11 +1,8 @@
-﻿using StringCalculator.Domain.Classes;
-
-namespace StringCalculator.Domain.Interfaces
+﻿namespace StringCalculator.Domain.Interfaces
 {
     public interface IStringCalculationResult
     {
         int CalculationResult { get; }
         string Formula { get; }
-        void ProcessTerm(Term term);
     }
 }
